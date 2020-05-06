f12fc81271c112bb8c7e554fae9ba6ca09a919a5
Artem Trepalin <artemtrepalin@gmail.com>
2020-05-06 22:50:26 +0300

    fix StringTest
    add Junit asserts

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 
 import java.util.Date;
 
-import static org.hamcrest.CoreMatchers.is;
 
 public class StringTest {
 
@@ -20,117 +19,82 @@ public class StringTest {
 
     @Test
     public void testConcatenation() {
-        Assert.assertThat((String)null + (String)null, is("nullnull"));
-        Assert.assertThat(("ABC" + null), is("ABCnull"));
-        Assert.assertThat(((String)null + "ABC"), is("nullABC"));
-        Assert.assertThat(("ABC" + "ABC"), is("ABCABC"));
+        Assert.assertEquals("nullnull", (String) null + (String) null);
+        Assert.assertEquals("ABCnull", "ABC" + null);
+        Assert.assertEquals("nullABC", (String) null + "ABC");
+        Assert.assertEquals("ABCABC", "ABC" + "ABC");
     }
 
 
     @Test
     public void testStringLength() {
-        Assert.assertThat(("ABC".length()), is(3));
+        Assert.assertEquals(3, "ABC".length());
     }
 
     @Test
     public void testStringIsEmpty() {
-        Assert.assertThat(("".isEmpty()), is(true));
+        Assert.assertEquals(true, "".isEmpty());
     }
 
     @Test
-    public void testStringEqualsTrue() {
-        Assert.assertThat(("test".equals("test")), is(true));
+    public void testStringEquals() {
+        Assert.assertEquals(true, "test".equals("test"));
+        Assert.assertEquals(false, "test".equals("TEST"));
     }
 
-    @Test
-    public void testStringEqualsFalse() {
-        Assert.assertThat(("test".equals("TEST")), is(false));
-    }
 
     @Test
     public void testStringEqualsIgnoreCase() {
-        String a = "test";
-        String b = "TeSt";
-        Assert.assertThat(("test".equalsIgnoreCase("TeSt")), is(true));
+        Assert.assertEquals(true, "test".equalsIgnoreCase("TeSt"));
     }
 
     @Test
     public void testStringToString() {
-        int[] m = {1, 2, 3};
-        Assert.assertThat((m.toString()), is("[I@23fe1d71"));
+        Assert.assertEquals("abc", "abc".toString());
     }
 
     @Test
     public void testStringCharAtTrue() {
-        String a = "test";
-        Assert.assertThat(a.charAt(0), is('t'));
+        Assert.assertEquals('t', "test".charAt(0));
     }
 
     @Test
-    public void testStringStartsWithTrue() {
-        String a = "Learning Java";
-        Assert.assertTrue(a.startsWith("Learn"));
-    }
-
-    @Test
-    public void testStringStartsWithFalse() {
+    public void testStringStartsWith() {
+        Assert.assertTrue("Learning Java".startsWith("Learn"));
         Assert.assertFalse("Learning Java".startsWith("Java"));
     }
+
 
     @Test
     public void testStringEndsWithTrue() {
         Assert.assertTrue("Learning Java Junior".endsWith("or"));
-    }
-
-    @Test
-    public void testStringEndsWithFalse() {
         Assert.assertFalse("Learning Java Junior".endsWith("Jun"));
     }
 
     @Test
     public void testStringContainsTrue() {
         Assert.assertTrue("Learn about String API by unit testing the following String methods".contains("by unit testing the following"));
-    }
-
-    @Test
-    public void testStringContainsFalse() {
         Assert.assertFalse("Learn about String API by unit testing the following String methods".contains("concatenation"));
+
     }
 
     @Test
     public void testStringConcat() {
-        Assert.assertThat(("Learn about String API by unit testing the following String methods".concat("JAVA")),
-                is("Learn about String API by unit testing the following String methodsJAVA"));
+        Assert.assertEquals("Learn about String API by unit testing the following String methodsJAVA",
+                "Learn about String API by unit testing the following String methods".concat("JAVA"));
     }
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void testStringValueOf() {
-        Assert.assertThat((String.valueOf(12345)), is("12345"));
-    }
-
-    @Test
-    public void testStringValueOfBool() {
-        Assert.assertThat((String.valueOf(true)), is("true"));
-
-    }
-
-    @Test
-    public void testStringValueOfObj() {
         Date date = new Date(1588766615);
-        Assert.assertThat((String.valueOf(date)), is("Mon Jan 19 12:19:26 MSK 1970"));
-    }
-
-    // I dont now how testing null
-    //@Test
-    public void testStringValueNull() {
-        String.valueOf(null);
-        Assert.assertNotNull(NullPointerException.class);
-
+        Assert.assertEquals("12345", String.valueOf(12345));
+        Assert.assertEquals("true", String.valueOf(true));
+        Assert.assertEquals("Mon Jan 19 12:19:26 MSK 1970", String.valueOf(date));
+        Assert.assertEquals(NullPointerException.class, String.valueOf(null));
     }
 
     @Test
     public void testStringReplace() {
-        String s = "Java";
-        Assert.assertThat((s.replace("a", "e")), is("Jeve"));
+        Assert.assertEquals("Jeve", "Java".replace("a", "e"));
     }
 }
